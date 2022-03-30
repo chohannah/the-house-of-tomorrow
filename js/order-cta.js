d188720c74cf6b1a0f9618071da59e6eba7dbec2
Jinnie Cho <yejincho1105@gmail.com>
2022-03-31 00:48:46 +0900

    ♻️ Changed add and remove mothods to setAttribute method and set aria-label on change

--- js/order-cta.js
+++ js/order-cta.js
@@ -19,27 +19,21 @@ function closeOrderFormModal() {
 orderFormModalOverlay.addEventListener('click', closeOrderFormModal)
 
 function toggleOrderCtaBookmark() {
-  // 1. 버튼에 .is-active 더하기
-  // 2. icon-bookmark-filled로 아이콘 변경
-  // 3. 카운트 숫자 값을 변경
   const [icon, countSpan] = this.children
   const count = Number(countSpan.innerText.replaceAll(',', ''))
 
   let newCount = count
 
   if (this.classList.contains('is-active')) {
-    // NOTE: 활성화가 된 상태 -> 비활성화 시켜야 됨 (.ic-bookmark) -> -1 해줘야함
-    icon.classList.add('ic-bookmark')
-    icon.classList.remove('ic-bookmark-filled')
+    icon.setAttribute('class', 'ic-bookmark')
     newCount = newCount - 1
   } else {
-    // NOTE: 비활성화 된 상태 -> 활성화 시켜야 됨 (.ic-bookmark-filled ) -> +1 해줘야함
-    icon.classList.add('ic-bookmark-filled')
-    icon.classList.remove('ic-bookmark')
+    icon.setAttribute('class', 'ic-bookmark-filled')
     newCount = newCount + 1
   }
 
-  countSpan.innerText = newCount.toLocaleString
+  countSpan.innerText = newCount.toLocaleString()
+  countSpan.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`)
 
   this.classList.toggle('is-active')
 }
